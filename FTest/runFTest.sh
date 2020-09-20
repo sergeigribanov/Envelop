f0b10ce05ec7ba04541e747a2a540da4e86702ee
Sergei Gribanov <ssgribanov@gmail.com>
2020-09-20 23:07:50 +0700

    A number of variables have been replaced

--- FTest/runFTest.sh
+++ FTest/runFTest.sh
@@ -1,22 +1,22 @@
 #
-for var in ws_2016-17-18_ttHon0.26_2D_31012020
+for var in ws_run2_ttHon0.26_2D_SM_ggHHonly_04062020
 do
 ext=${var}_mgg
 dir=${var} 
-/afs/cern.ch/user/i/ivovtin/HHggbb/CMSSW_8_1_0/src/flashggFinalFit/Background/bin/fTest -i /afs/cern.ch/user/i/ivovtin/HHggbb/CMSSW_8_1_0/src/HiggsAnalysis/bbggLimits2018/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D IVAN_${ext} -d IVAN_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
-eosdir=/eos/user/i/ivovtin/www/HHbbgg/fTest/${ext}
+/afs/cern.ch/user/s/segriban/test_Hggbb/CMSSW_8_1_0/src/HiggsAnalysis/flashggFinalFit/Background/bin/fTest -i /afs/cern.ch/user/s/segriban/test_Hggbb/CMSSW_8_1_0/src/HiggsAnalysis/bbggLimits2018/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D SERGEI_${ext} -d SERGEI_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
+eosdir=/afs/cern.ch/user/s/segriban/test_Hggbb/www/HHbbgg/fTest/${ext}
 mkdir -p ${eosdir}
-cp IVAN_${ext}/*png ${eosdir}
+cp SERGEI_${ext}/*png ${eosdir}
 cp fTestRes_${ext}.dat ${eosdir}
-cp /eos/user/i/ivovtin/www/tmp/index.php ${eosdir}
+cp /afs/cern.ch/user/s/segriban/test_Hggbb/www/tmp/index.php ${eosdir}
 
 ext=${var}_mjj
-/afs/cern.ch/user/i/ivovtin/HHggbb/CMSSW_8_1_0/src/flashggFinalFit/Background/bin/fTest -i /afs/cern.ch/user/i/ivovtin/HHggbb/CMSSW_8_1_0/src/HiggsAnalysis/bbggLimits2018/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D IVAN_${ext} -d IVAN_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 0 &> fTestRes_${ext}.dat
-eosdir=/eos/user/i/ivovtin/www/HHbbgg/fTest/${ext}
+/afs/cern.ch/user/s/segriban/test_Hggbb/CMSSW_8_1_0/src/HiggsAnalysis/flashggFinalFit/Background/bin/fTest -i /afs/cern.ch/user/s/segriban/test_Hggbb/CMSSW_8_1_0/src/HiggsAnalysis/bbggLimits2018/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D SERGEI_${ext} -d SERGEI_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 0 &> fTestRes_${ext}.dat
+eosdir=/afs/cern.ch/user/s/segriban/test_Hggbb/www/HHbbgg/fTest/${ext}
 mkdir -p ${eosdir}
-cp IVAN_${ext}/*png ${eosdir}
+cp SERGEI_${ext}/*png ${eosdir}
 cp fTestRes_${ext}.dat ${eosdir}
-cp /eos/user/i/ivovtin/www/tmp/index.php ${eosdir}
+cp /afs/cern.ch/user/s/segriban/test_Hggbb/www/tmp/index.php ${eosdir}
 done
 
 
@@ -25,16 +25,16 @@ done
 #ext0=2D_Y2_BS
 #ext=${ext0}_${var}_mgg
 #dir=${ext0}/ws_2016-17_${var} 
-#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D IVAN_${ext} -d IVAN_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
+#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D SERGEI_${ext} -d SERGEI_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
 #mkdir ${eosdir}
-#cp IVAN_${ext}/*png ${eosdir}
+#cp SERGEI_${ext}/*png ${eosdir}
 #cp fTestRes_${ext}.dat ${eosdir}
 #cp /eos/user/l/lkardapo/www/tmp/index.php ${eosdir}
 #
 #ext=${ext0}_${var}_mjj
-#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D IVAN_${ext} -d IVAN_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 0 &> fTestRes_${ext}.dat
+#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D SERGEI_${ext} -d SERGEI_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 0 &> fTestRes_${ext}.dat
 #mkdir ${eosdir}
-#cp IVAN_${ext}/*png ${eosdir}
+#cp SERGEI_${ext}/*png ${eosdir}
 #cp fTestRes_${ext}.dat ${eosdir}
 #cp /eos/user/l/lkardapo/www/tmp/index.php ${eosdir}
 #done
@@ -49,10 +49,10 @@ done
 #ext0=1D_Y3
 #ext=${ext0}_${var}
 #dir=${ext0}/ws_2016-17_${var} 
-#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D IVAN_${ext} -d IVAN_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
+#./bin/fTest -i /afs/cern.ch/work/i/ivovtin/public/ForLeonid/${dir}/Node_SM/ws_hhbbgg.data_bkg.root --saveMultiPdf CMS-HGG_multipdf_${ext}.root  -D SERGEI_${ext} -d SERGEI_${ext}/res.dat -f cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11 --isData 1 -c 12 --ismgg 1 &> fTestRes_${ext}.dat
 #eosdir=/eos/user/l/lkardapo/www/HHbbgg/fTest/${ext}
 #mkdir ${eosdir}
-#cp IVAN_${ext}/*png ${eosdir}
+#cp SERGEI_${ext}/*png ${eosdir}
 #cp fTestRes_${ext}.dat ${eosdir}
 #cp /eos/user/l/lkardapo/www/tmp/index.php ${eosdir}
 #done
