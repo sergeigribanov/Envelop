0debfd89fb5f9becca85e223862cda8c110531e4
Sergei Gribanov <ssgribanov@gmail.com>
2020-09-22 15:47:53 +0700

    Array dimensions have been changed

--- Indexes.C
+++ Indexes.C
@@ -1,8 +1,8 @@
 void Indexes(TString path, TString MultidimFitFile = "higgsCombineTest.MultiDimFit.mH120.root"){
    TFile *f = new TFile(path+"/Node_SM/ws_hhbbgg.data_bkg_multipdf.root");
    RooWorkspace *w_bias = (RooWorkspace*)f->Get("w_bias");
-   int NCAT=14;
-   int max_ind[14];
+   int NCAT=12;
+   int max_ind[12];
    cout<<"max_ind = [";
    for(int i=0;i<NCAT;i++){
       cout<<((RooMultiPdf*)w_bias->pdf(Form("roomultipdf_cat%d",i)))->getNumPdfs();
@@ -11,15 +11,19 @@ void Indexes(TString path, TString MultidimFitFile = "higgsCombineTest.MultiDimF
       if(i<NCAT-1) cout<<",";}
    cout<<"]"<<endl;
 
-   TFile *f2 = new TFile(MultidimFitFile.Data());
+   TFile *f2 = new TFile(path + "/" + MultidimFitFile.Data());
    TTree *tr = (TTree*)f2->Get("limit");
-   tr->Draw("pdf_index_cat0:pdf_index_cat1:pdf_index_cat2:pdf_index_cat3:pdf_index_cat4:pdf_index_cat5:pdf_index_cat6:pdf_index_cat7:pdf_index_cat8:pdf_index_cat9:pdf_index_cat10:pdf_index_cat11:pdf_index_cat12:pdf_index_cat13","","PARA goff");
+   tr->Draw("pdf_index_cat0:pdf_index_cat1:pdf_index_cat2:pdf_index_cat3:pdf_index_cat4:pdf_index_cat5:pdf_index_cat6:pdf_index_cat7:pdf_index_cat8:pdf_index_cat9:pdf_index_cat10:pdf_index_cat11","","PARA goff");
    //tr->Draw("pdf_index_cat0:pdf_index_cat1:pdf_index_cat2:pdf_index_cat3:pdf_index_cat4:pdf_index_cat5:pdf_index_cat6:pdf_index_cat7:pdf_index_cat8:pdf_index_cat9:pdf_index_cat10:pdf_index_cat11:pdf_index_cat12:pdf_index_cat13:pdf_index_cat14:pdf_index_cat15","","PARA goff");
-   double ind[14] = {
+   cout << "POINT 0" << endl;
+   tr->Print();
+   cout << "POINT 1" << endl;
+   double tmpVar = (tr->GetVal(0))[0];
+   cout << "POINT 2" << endl;
+   double ind[12] = {
       (tr->GetVal(0))[0], (tr->GetVal(1))[0], (tr->GetVal(2))[0], (tr->GetVal(3))[0], (tr->GetVal(4))[0], (tr->GetVal(5))[0],
-      (tr->GetVal(6))[0], (tr->GetVal(7))[0], (tr->GetVal(8))[0], (tr->GetVal(9))[0], (tr->GetVal(10))[0], (tr->GetVal(11))[0],
-      (tr->GetVal(12))[0], (tr->GetVal(13))[0]
-   };
+      (tr->GetVal(6))[0], (tr->GetVal(7))[0], (tr->GetVal(8))[0], (tr->GetVal(9))[0], (tr->GetVal(10))[0], (tr->GetVal(11))[0]};
+   cout << "POINT 3" << endl;
    //double ind[16] = {
   //    (tr->GetVal(0))[0], (tr->GetVal(1))[0], (tr->GetVal(2))[0], (tr->GetVal(3))[0], (tr->GetVal(4))[0], (tr->GetVal(5))[0],
   //    (tr->GetVal(6))[0], (tr->GetVal(7))[0], (tr->GetVal(8))[0], (tr->GetVal(9))[0], (tr->GetVal(10))[0], (tr->GetVal(11))[0],
@@ -43,7 +47,7 @@ void Indexes(TString path, TString MultidimFitFile = "higgsCombineTest.MultiDimF
    cout<<endl;
    int MaxInd = TMath::MaxElement(NCAT,max_ind);
    //cout<<"TString funcname[16]["<<MaxInd<<"]={"<<endl;   
-   cout<<"TString funcname[14]["<<MaxInd<<"]={"<<endl;   
+   cout<<"TString funcname[12]["<<MaxInd<<"]={"<<endl;   
    for(int i=0;i<NCAT;i++){
       cout<<"{ ";
       for(int j=0;j<MaxInd;j++){
